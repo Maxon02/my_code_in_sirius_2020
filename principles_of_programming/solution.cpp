dcdef4eae63ea65206f72c5181ef248a3b00f5e8
Maxon02 <55329921+Maxon02@users.noreply.github.com>
2020-01-20 20:29:25 +0300

    Update solution.cpp

--- principles_of_programming/solution.cpp
+++ principles_of_programming/solution.cpp
@@ -1,16 +1,20 @@
-/*#include <iostream> wrong code, right code will be in the evening
+#include <iostream>
 #include <vector>
 #include <string>
 #include <fstream>
 
 using namespace std;
 
-const int num_of_tests = 7;
-
 struct subst{ //subst = substitution
     string from, to;
     bool isFinal;
 };
+
+void printAnswer(string ans){
+    ofstream cout ("output.txt", ios::out);
+    cout << ans;
+    cout.close();
+}
 
 bool check_correct(int test, string my_ans){   
     ifstream cin ("answer" + to_string(test) + ".txt", ios::in); 
@@ -20,13 +24,8 @@ bool check_correct(int test, string my_ans){
     return my_ans == right_ans;
 }
 
-void print_verdict(int test, string verdict){
-    ofstream cout ("output.txt", test == 1 ? ios::out : ios::app);
-    cout << verdict << '\n';
-}
-
-void parseSchema(int test, int &amount, vector <subst> &schema, string &word) {
-    ifstream cin ("input" + to_string(test) + ".txt", ios::in);
+void parseSchema(int test, int &amount, vector <subst> &schema, string &word) { //test: 0 - user input, [1..7] - local tests
+    ifstream cin ("input" + (test ? to_string(test) : "") + ".txt", ios::in);
     cin >> amount;
     schema.resize(amount);
     for (int i = 0; i < amount; i++){
@@ -82,7 +81,7 @@ string applySchema(int amount, vector <subst> schema, string word){
     return word;
 }
 
-bool testApplySchemaOnce(){
+bool testApplySchemaOnce(){ // 3 tests
     vector <subst> schema1;
     subst fst = {"ba", "ab", false};
     schema1.push_back(fst);
@@ -98,21 +97,36 @@ bool testApplySchemaOnce(){
     return true;
 }
 
-int main() {
-    if (!testApplySchemaOnce()){
-        cout << "Error in applySchemaOnce";
-        return 0;
-    }
+const int num_of_tests = 7;
+
+bool testApplySchema(){ // 7 tests
     for (int i = 1; i <= num_of_tests; i++){ 
         string word;
         int amount;
         vector <subst> schema;
         parseSchema(i, amount, schema, word);
         string answer = applySchema(amount, schema, word);
-        if (check_correct(i, answer))
-            print_verdict(i, "Correct");
-        else
-            print_verdict(i, "Wrong answer");
+        if (!check_correct(i, answer))
+            return false;
     }
+    return true;
+}
+
+int main() {
+    if (!testApplySchemaOnce()){
+        printAnswer("Error in applySchemaOnce");
+        return 0;
+    }
+    if (!testApplySchema()){
+        printAnswer("Error in applySchema");
+        return 0;
+    }
+    string word;
+    int amount;
+    vector <subst> schema;
+    parseSchema(0, amount, schema, word); //user must enter text in input.txt, answer is in output.txt
+    string answer = applySchema(amount, schema, word);
+    printAnswer(answer);
+
     return 0;
-}*/
+}
